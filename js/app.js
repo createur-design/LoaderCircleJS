0711aeec1fe3ba6957a4cd28943d3fe4c9f3d8e3
Christophe Delobele <cdelobel.ext@simplon.co>
2019-01-29 16:52:35 +0100

    amelioration

--- js/app.js
+++ js/app.js
@@ -1,19 +1,19 @@
-function init(){
-    console.log("Document chargé !");
+function init(){    
 
+    var loaded = false;
     var mousedownID = -1;  //Global ID of mouse down interval
     var degrease;
     var count = 0;
     var vitesse = 10;
-    var loaded = false;
+
+    var element = document.querySelector("#empreinte");
 
     var bar = document.querySelector("#bar");
 
     function mousedown(event) {
         if(mousedownID==-1) { //Prevent multimple loops!
             clearInterval(degrease);
-            mousedownID = setInterval(whilemousedown, vitesse /*execute every 100ms*/);
-            
+            mousedownID = setInterval(whilemousedown, vitesse /*execute every 100ms*/);            
         }
     }
     
@@ -24,59 +24,62 @@ function init(){
             }     
             clearInterval(mousedownID);
             mousedownID=-1;
-            degrease = setInterval(whileMouseUp, vitesse); 
-            
+            degrease = setInterval(whileMouseUp, vitesse);             
         }
     }
 
     function whileMouseUp() {
         count -= 1; 
-        bar.style.width = count + "%";   
-        arcMove();    
-        console.log(count);
         if(count < 1){
             console.log("Stop");
             clearInterval(degrease);
         }
+        bar.style.width = count + "%";   
+        arcMove();  
+    }
+
+
+    function load(){
+        var loader = document.querySelector("#loader");
+
+        loader.style.display = "none";
     }
 
     function whilemousedown() { 
         count += 1; 
-        bar.style.width = count + "%";
-        arcMove();
         if(count > 100){
             console.log("Loaded");
-            loaded = true;
+            loaded = true;            
             clearInterval(degrease);
             clearInterval(mousedownID);
+            alert("à vous de faire la suite :-)");
+            console.log("Document chargé !");
+            load();
             return;            
         }
-        console.log(count);
+        bar.style.width = count + "%";
+        arcMove();
     }
 
     //Assign events
-    document.addEventListener("mousedown", mousedown);
-    document.addEventListener("mouseup", mouseup);
+    element.addEventListener("mousedown", mousedown);
+    element.addEventListener("touchstart", mousedown);
+    element.addEventListener("mouseup", mouseup);
+    element.addEventListener("touchend", mouseup);
     //Also clear the interval when user leaves the window with mouse
     //document.addEventListener("mouseout", mouseup);
 
 
-
-
-
-
-
     var can = document.getElementById('canvas'),
-      spanProcent = document.getElementById('procent'),
-       c = can.getContext('2d');
+        spanProcent = document.getElementById('procent'),
+        c = can.getContext('2d');
  
   var posX = can.width / 2,
-      posY = can.height / 2,
+      posY = can.height / 2,      
       fps = 1000 / 200,
       procent = 0,
       oneProcent = 360 / 100,
       result = oneProcent * count;
-      console.log("result : " + result);
   
   c.lineCap = 'round';
   //arcMove();
@@ -86,24 +89,44 @@ function init(){
       deegres = count * oneProcent;
       c.clearRect( 0, 0, can.width, can.height );
       procent = deegres / oneProcent;
+    
+    var tailleCercle = 70;  
+    var borderCercle = 5;
+    var color = "#006DF0";
 
-      spanProcent.innerHTML = procent.toFixed();
+    var progressCircle = (Math.PI/180) * (270 + deegres);
 
-      c.beginPath();
+    var pointEncrage = (Math.PI/180) * 270;
+
+    if (progressCircle < 5){
+        color = "transparent";
+    }
+
+    if(procent >= 100){
+        procent = 100;
+        color = "green";
+    }
+
+    spanProcent.innerHTML = procent.toFixed() + "%";
+    if(procent <= 0){
+        spanProcent.innerHTML = "";
+    }
+
+      /*c.beginPath();
       c.arc( posX, posY, 70, (Math.PI/180) * 270, (Math.PI/180) * (270 + 360) );
       c.strokeStyle = '#b1b1b1';
       c.lineWidth = '10';
-      c.stroke();
+      c.stroke();*/
 
       c.beginPath();
-      c.strokeStyle = '#3949AB';
-      c.lineWidth = '10';
-      c.arc( posX, posY, 70, (Math.PI/180) * 270, (Math.PI/180) * (270 + deegres) );
-      c.stroke();
+      c.strokeStyle = color;
+      c.lineWidth = borderCercle;
+      c.arc( posX, posY, tailleCercle, pointEncrage, progressCircle );
+      c.stroke();      
 
-    
   }
-
     
 
 }
+
+init();
